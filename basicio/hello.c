6a280dc9940a86cfe4225f4c2d6e09323ddbb2f8
flatychen <flatychen@126.com>
2014-11-30 18:53:54 +0800

    hello world test

---
+++ basicio/hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author:flatychen 
+	> Mail: 
+	> Created Time: 2014年11月30日 星期日 16时51分24秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    puts("hello,world");
+    printf("printf hello,world");
+}
